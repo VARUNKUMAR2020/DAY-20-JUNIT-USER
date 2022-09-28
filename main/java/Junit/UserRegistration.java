4779a617834d663886eac7d360efb4913f6bd404
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-28 13:51:43 +0530

    USER REGISTRATION

---
+++ main/java/Junit/UserRegistration.java
@@ -0,0 +1,9 @@
+package Junit;
+
+public class UserRegistration 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "USER REGISTRATION" );
+    }
+}
---
+++ test/java/Junit/TestUserRegistration.java
@@ -0,0 +1,14 @@
+package Junit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestUserRegistration 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
