cadb70f06846a1a17363fb643c34be34543032df
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-29 15:28:03 +0530

    ADD: VALIDATION OF FIRST NAME

--- main/java/Junit/UserRegistration.java
+++ main/java/Junit/UserRegistration.java
@@ -1,9 +1,27 @@
 package Junit;
 
-public class UserRegistration 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "USER REGISTRATION" );
-    }
+import java.util.*;
+import java.util.regex.*;
+
+public class UserRegistration {
+	public static String FirstName(String fName) {
+		Pattern name = Pattern.compile("[A-Z][a-z]{2,}");
+		Matcher checkName = name.matcher(fName);
+		if (checkName.find()) {
+			System.out.println("THANK YOU FOR ENTERING VALID USERNAME "); 
+		} else {
+			System.err.println("YOU HAVE ENTERED INVALID USERNAME ");
+		}
+		return fName;
+	}
+
+	public static void main(String[] args) {
+		UserRegistration CheckName = new UserRegistration();
+		System.out.println("USER REGISTRATION");
+		Scanner firstName = new Scanner(System.in);
+		System.out.println("ENTER THE USER NAME: ");
+		String Name = firstName.nextLine();
+		CheckName.FirstName(Name);
+
+	}
 }
--- test/java/Junit/TestUserRegistration.java
+++ test/java/Junit/TestUserRegistration.java
@@ -1,14 +1,15 @@
 package Junit;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestUserRegistration 
-{
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class TestUserRegistration {
+	@Test
+	public void userNameTestCase() {
+
+		String Expected = UserRegistration.FirstName("Varun");
+		assertEquals("Varun", Expected);
+
+	}
 }
