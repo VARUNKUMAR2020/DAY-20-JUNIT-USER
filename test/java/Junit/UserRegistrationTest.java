d59ca32b2741b214d8a9e6ea2d4d98f1b4ee0975
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-10-03 05:28:29 +0530

    ADD: RULE3-SHOULD HAVE ATLEAST 1 NUMERIC NUMBER

--- test/java/Junit/UserRegistrationTest.java
+++ test/java/Junit/UserRegistrationTest.java
@@ -31,8 +31,8 @@ public class UserRegistrationTest {
 	}
 	@Test
 	public void passwordTestCase() {
-		String Expected4 = Test.Password("VVsuwjsi");
-		Assert.assertEquals("VVsuwjsi", Expected4);
+		String Expected4 = Test.Password("VVs5uwjsi");
+		Assert.assertEquals("VVs5uwjsi", Expected4);
 	}
 }
 
